7133881c9237788f7029e7628d1a1938a6094502
samson205 <abeb>
2025-10-17 17:46:30 +0300

    fix graphs; add func delete_images

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
 import re
+from typing import Union
+
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 from pdf_generator import PDFGenerator
 
-def read_file(file_name):
+
+def read_file(file_name: str):
     with open(file_name, 'r', encoding="utf-8") as file:
         lines = file.readlines()
 
@@ -54,7 +60,7 @@ def read_file(file_name):
     return False, mode, total_blocks, False
 
 
-def data_processed(data):
+def data_processed(data: str) -> Union[dict, None]:
     pattern = r'(\d+\.?\d*)%\s+done,\s+((\d+):)?(\d+):(\d+)\s+elapsed.*?\((\d+)/(\d+)/(\d+)\s+errors\)'
 
     match = re.search(pattern, data)
@@ -77,10 +83,11 @@ def data_processed(data):
     return None
 
 
-def calculate_speed(test):
+def calculate_speed(test: dict) -> pd.DataFrame:
     data = test['data']
-    if not data:
-        return None
+    # if not data:
+    #     return None
+
     df = pd.DataFrame(data)
     df['total_time'] -= df['total_time'].iloc[0]
     df['blocks_processed'] = test['total_blocks'] * (df['percent'] / 100)
@@ -90,116 +97,130 @@ def calculate_speed(test):
     df['time_delta'] = df['total_time'].diff()
     df = df[df['time_delta'] > 0]
 
-    df['bytes_per_second'] = (df['blocks_delta'] * 1024) / df['time_delta']
-    df['kilobytes_per_second'] = df['bytes_per_second'] / 1024
-    df['megabytes_per_second'] = df['kilobytes_per_second'] / 1024
-
-    # df['speed_smoothed'] = df['megabytes_per_second'].rolling(
-    #     window=5, center=True, min_periods=1
-    # ).mean()
+    df['b_per_sec'] = (df['blocks_delta'] * 1024) / df['time_delta']
+    df['kb_per_sec'] = df['b_per_sec'] / 1024
+    df['mb_per_sec'] = df['kb_per_sec'] / 1024
 
     df['test_name'] = test['name']
 
     return df
 
 
-def plots(df_tests: list):
+def plots(df_tests: list) -> None:
+    if not os.path.exists("temp"):
+        os.mkdir("temp")
+
     for i, current_test in enumerate(df_tests):
         fig1, ax1 = plt.subplots(figsize=(18, 10))
-        if len(current_test['total_time']) < 3000:
-            step = 2
-        elif len(current_test['total_time']) < 6000:
-            step = 5
-        else:
-            step = 10
-        ax1.plot(current_test['total_time'][::step], current_test['megabytes_per_second'][::step])
-        ax1.set_title(current_test['test_name'].iloc[0] + " (зависимость скорости от времени)")
-        ax1.set_xlabel('Секунды')
-        ax1.set_ylabel('МБ/сек')
+        new_x = np.linspace(0, current_test['total_time'].iloc[-1], 1500)
+        new_y = np.interp(new_x, current_test['total_time'], current_test['mb_per_sec'])
+        ax1.plot(new_x, new_y)
+        ax1.set_title(current_test['test_name'].iloc[0] + " (зависимость скорости от времени)", fontsize=20)
+        ax1.set_xlabel('Время, сек', fontsize=18)
+        ax1.set_ylabel('Скорость, МБ/сек', fontsize=18)
         ax1.grid(which='minor', color='0.85')
         ax1.minorticks_on()
         ax1.grid(which='major', color='0.5')
-        fig1.savefig(f'test{i + 1}.png')
+        ax1.tick_params(axis='both', which='major', labelsize=16)
+        ax1.set_ylim(ymin=0)
+        fig1.savefig(f"temp/test{i + 1}.png")
 
         fig2, ax2 = plt.subplots(figsize=(18, 10))
-        ax2.plot(current_test['percent'][::step], current_test['megabytes_per_second'][::step])
-        ax2.set_title(current_test['test_name'].iloc[0] + " (зависимость скорости от объема)")
-        ax2.set_xlabel('Объем накопителя в %')
-        ax2.set_ylabel('МБ/сек')
+        new_x = np.linspace(0, current_test['percent'].iloc[-1], 1500)
+        new_y = np.interp(new_x, current_test['percent'], current_test['mb_per_sec'])
+        ax2.plot(new_x, new_y)
+        ax2.set_title(current_test['test_name'].iloc[0] + " (зависимость скорости от объема)", fontsize=20)
+        ax2.set_xlabel('Объем накопителя в %', fontsize=18)
+        ax2.set_ylabel('Скорость, МБ/сек', fontsize=18)
         ax2.grid(which='minor', color='0.85')
         ax2.minorticks_on()
         ax2.grid(which='major', color='0.5')
         ax2.ticklabel_format(style='plain')
-        fig2.savefig(f'test{i + 1}_percent.png')
+        ax2.tick_params(axis='both', which='major', labelsize=16)
+        ax2.set_ylim(ymin=0)
+        fig2.savefig(f"temp/test{i + 1}_percent.png")
 
 
-def mean_speed(df_tests):
-    all_write_speeds = pd.concat(df_tests[i]['megabytes_per_second'] for i in range(0, len(df_tests), 2))
-    all_read_speeds = pd.concat(df_tests[i]['megabytes_per_second'] for i in range(1, len(df_tests), 2))
+def delete_images(folder_path="temp") -> None:
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        except Exception:
+            print(f"Ошибка при удалении файла {file_path}")
+
+
+def mean_speed(df_tests: list):
+    all_write_speeds = pd.concat(df_tests[i]['mb_per_sec'] for i in range(0, len(df_tests), 2))
+    all_read_speeds = pd.concat(df_tests[i]['mb_per_sec'] for i in range(1, len(df_tests), 2))
     return round(all_write_speeds.mean(), 1), round(all_read_speeds.mean(), 1)
 
 
-def min_speed(df_tests):
-    all_write_speeds = pd.concat(df_tests[i]['megabytes_per_second'] for i in range(0, len(df_tests), 2))
-    all_read_speeds = pd.concat(df_tests[i]['megabytes_per_second'] for i in range(1, len(df_tests), 2))
+def min_speed(df_tests: list):
+    all_write_speeds = pd.concat(df_tests[i]['mb_per_sec'] for i in range(0, len(df_tests), 2))
+    all_read_speeds = pd.concat(df_tests[i]['mb_per_sec'] for i in range(1, len(df_tests), 2))
     return round(all_write_speeds.min(), 1), round(all_read_speeds.min(), 1)
 
 
-def max_speed(df_tests):
-    all_write_speeds = pd.concat(df_tests[i]['megabytes_per_second'] for i in range(0, len(df_tests), 2))
-    all_read_speeds = pd.concat(df_tests[i]['megabytes_per_second'] for i in range(1, len(df_tests), 2))
+def max_speed(df_tests: list):
+    all_write_speeds = pd.concat(df_tests[i]['mb_per_sec'] for i in range(0, len(df_tests), 2))
+    all_read_speeds = pd.concat(df_tests[i]['mb_per_sec'] for i in range(1, len(df_tests), 2))
     return round(all_write_speeds.max(), 1), round(all_read_speeds.max(), 1)
 
 
-def mean_cycle_time(df_tests):
+def mean_cycle_time(df_tests: list):
     all_write_time = [df_tests[i]['total_time'].iloc[-1] for i in range(0, len(df_tests), 2)]
     all_read_time = [df_tests[i]['total_time'].iloc[-1] for i in range(1, len(df_tests), 2)]
     cycles = [all_write_time[i] + all_read_time[i] for i in range(len(all_write_time))]
     mean_time = round(sum(cycles) / len(cycles))
     spread = round(((max(cycles) - min(cycles)) / mean_time) * 100, 2)
-    return f'{mean_time // 3600}:{(mean_time % 3600) // 60}:{mean_time % 60}\n{spread}%'
+    return f"{mean_time // 3600}:{(mean_time % 3600) // 60}:{mean_time % 60}\n{spread}%"
 
 
-
-def main(file_name):
-    tests, name, blocks, complete = read_file(f'data/{file_name}')
+def main(file_name: str) -> bool:
+    tests, name, blocks, complete = read_file(f"data/{file_name}")
     if complete:
         df_tests = [calculate_speed(tests[i]) for i in range(len(tests))]
         plots(df_tests)
-        generator = PDFGenerator(f'{file_name.split('.')[0]}.pdf')
+        generator = PDFGenerator(f"{file_name.split('.')[0]}.pdf")
         generator.add_title()
-        time_string = f'{df_tests[-1]['hours'].iloc[-1]}:{df_tests[-1]['minutes'].iloc[-1]}:{df_tests[-1]['seconds'].iloc[-1]}'
+        time_string = f"{df_tests[-1]['hours'].iloc[-1]}:{df_tests[-1]['minutes'].iloc[-1]}:{df_tests[-1]['seconds'].iloc[-1]}"
         max_write, max_read = max_speed(df_tests)
         min_write, min_read = min_speed(df_tests)
         mean_write, mean_read = mean_speed(df_tests)
         mean_time = mean_cycle_time(df_tests)
         generator.add_table([
-        ['Объем\nнакопителя', 'Режим\nтестирования', 'Результат\nтестирования', 'Общее\nвремя\nтестирования', 'Кол-во\nпроходов\nчтения и\nзаписи', 'Среднее\nвремя\nодного цикла\nчтения и\nзаписи', 'Средняя\nскорость\nчтения и\nзаписи', 'Минимальная\nскорость\nчтения и\nзаписи', 'Максимальная\nскорость чтения\nи записи'],
-        [f'{blocks} блоков\n({blocks * 1024} байт)',
-         name.capitalize(),
-         'Пройдено',
-         time_string,
-         f'{len(tests) // 2}',
-         mean_time,
-         f'{mean_read}; {mean_write} МБ/с\n{mean_read*1024}; {mean_write*1024} кБ/с\n{mean_read*1024*1024};\n{mean_write*1024*1024} байт/с',
-         f'{min_read}; {min_write} МБ/с\n{min_read*1024}; {min_write*1024} кБ/с\n{min_read*1024*1024};\n{min_write*1024*1024} байт/с',
-         f'{max_read}; {max_write} МБ/с\n{max_read*1024}; {max_write*1024} кБ/с\n{max_read*1024*1024};\n{max_write*1024*1024} байт/с']
-    ])
+            ['Объем\nнакопителя', 'Режим\nтестирования', 'Результат\nтестирования',
+             'Общее\nвремя\nтестирования', 'Кол-во\nпроходов\nчтения и\nзаписи',
+             'Среднее\nвремя\nодного цикла\nчтения и\nзаписи', 'Средняя\nскорость\nчтения и\nзаписи',
+             'Минимальная\nскорость\nчтения и\nзаписи', 'Максимальная\nскорость чтения\nи записи'],
+            [f'{blocks} блоков\n({blocks * 1024} байт)',
+             name.capitalize(),
+             'Пройдено',
+             time_string,
+             f"{len(tests) // 2}",
+             mean_time,
+             f"{mean_read}; {mean_write} МБ/с\n{mean_read * 1024}; {mean_write * 1024} кБ/с\n{mean_read * 1024 * 1024};\n{mean_write * 1024 * 1024} байт/с",
+             f"{min_read}; {min_write} МБ/с\n{min_read * 1024}; {min_write * 1024} кБ/с\n{min_read * 1024 * 1024};\n{min_write * 1024 * 1024} байт/с",
+             f"{max_read}; {max_write} МБ/с\n{max_read * 1024}; {max_write * 1024} кБ/с\n{max_read * 1024 * 1024};\n{max_write * 1024 * 1024} байт/с"]
+        ])
         for i in range(len(tests)):
-            generator.add_image(f'test{i+1}.png')
-            generator.add_image(f'test{i+1}_percent.png')
+            generator.add_image(f"temp/test{i + 1}.png")
+            generator.add_image(f"temp/test{i + 1}_percent.png")
         generator.generate()
+        delete_images()
 
-        return "Отчет успешно сгенерирован"
+        return True
     else:
-        generator = PDFGenerator(f'{file_name.split('.')[0]}.pdf')
+        generator = PDFGenerator(f"{file_name.split('.')[0]}.pdf")
         generator.add_title()
         generator.add_table([
             ['Объем\nнакопителя', 'Режим\nтестирования', 'Результат\nтестирования', 'Общее\nвремя\nтестирования',
              'Кол-во\nпроходов\nчтения и\nзаписи', 'Среднее\nвремя\nодного цикла\nчтения и\nзаписи',
              'Средняя\nскорость\nчтения и\nзаписи', 'Минимальная\nскорость\nчтения и\nзаписи',
              'Максимальная\nскорость чтения\nи записи'],
-            [f'{blocks} блоков\n({blocks * 1024} байт)',
+            [f"{blocks} блоков\n({blocks * 1024} байт)",
              name.capitalize(),
              'Провалено',
              '-',
@@ -212,9 +233,12 @@ def main(file_name):
 
         generator.generate()
 
-        return "Тест не был пройден"
+        return False
 
 
 if __name__ == "__main__":
     fname = input('Введите имя файла в папке data: ')
-    print(main(fname))
+    if main(fname):
+        print("Отчет успешно сгенерирован")
+    else:
+        print("Тест не был пройден")
--- pdf_generator.py
+++ pdf_generator.py
@@ -10,20 +10,25 @@ from reportlab.pdfbase.ttfonts import TTFont
 pdfmetrics.registerFont(TTFont('Arial', 'C:/Windows/Fonts/arial.ttf'))
 pdfmetrics.registerFont(TTFont('Arial-Bold', 'C:/Windows/Fonts/arialbd.ttf'))
 
-class PDFGenerator:
-    def __init__(self, file_name, title="Отчет"):
-        self.filename = file_name
-        self.title = title
-        self.elements = []
-        self.styles = getSampleStyleSheet()
-        self.doc = SimpleDocTemplate(file_name, pagesize=landscape(A4), topMargin=cm, bottomMargin=cm)
 
-    def add_title(self, text = None):
-        title_text = text if text else self.title
-        self.styles['Title'].fontName = 'Arial'
-        title = Paragraph(title_text, self.styles['Title'])
-        self.elements.append(title)
-        self.elements.append(Spacer(1, 20))
+class PDFGenerator:
+    _title = None
+    _elements = None
+    _styles = None
+    _doc = None
+
+    def __init__(self, file_name, title="Отчет"):
+        self._title = title
+        self._elements = []
+        self._styles = getSampleStyleSheet()
+        self._doc = SimpleDocTemplate(file_name, pagesize=landscape(A4), topMargin=cm, bottomMargin=cm)
+
+    def add_title(self, text=None):
+        title_text = text if text else self._title
+        self._styles['Title'].fontName = 'Arial'
+        title = Paragraph(title_text, self._styles['Title'])
+        self._elements.append(title)
+        self._elements.append(Spacer(1, 20))
 
     def add_table(self, data, style=None):
         table = Table(data)
@@ -38,20 +43,20 @@ class PDFGenerator:
             ])
 
         table.setStyle(style)
-        self.elements.append(table)
-        self.elements.append(Spacer(1, 20))
+        self._elements.append(table)
+        self._elements.append(Spacer(1, 20))
 
-    def add_image(self, image_path, width=11*inch, height=6*inch):
+    def add_image(self, image_path, width=11 * inch, height=6 * inch):
         try:
             if os.path.exists(image_path):
                 image = Image(image_path, width=width, height=height)
                 image.hAlign = 'CENTER'
-                self.elements.append(image)
-                self.elements.append(Spacer(1, 20))
+                self._elements.append(image)
+                self._elements.append(Spacer(1, 20))
                 return True
         except Exception as e:
             print(f"Произошла ошибка при добавлении изображения: {e}")
         return False
 
     def generate(self):
-        self.doc.build(self.elements)
+        self._doc.build(self._elements)
